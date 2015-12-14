532f04c3057a463cbf81b18bb0ff629c1502c595
Aleksandr Guljajev <gulyayev.alex@gmail.com>
2015-12-14 13:58:49 +0100

    Removing fonts

--- configuration.nix
+++ configuration.nix
@@ -123,22 +123,4 @@
 	virtualisation.docker.socketActivation = false;
 
 	boot.kernelPackages = pkgs.linuxPackages_4_3;
-
-	fonts = {
-		enableFontDir = true;
-		enableCoreFonts = true;
-		enableGhostscriptFonts = true;
-		fonts = with pkgs ; [
-			liberation_ttf
-				ttf_bitstream_vera
-				dejavu_fonts
-				terminus_font
-				bakoma_ttf
-				clearlyU
-				cm_unicode
-				andagii
-				bakoma_ttf
-		];
-	};
-
 }
