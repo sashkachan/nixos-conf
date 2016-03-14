bc45437f37435730152b8fcc70e5ef8477ba9865
Aleksandr Guljajev <gulyayev.alex@gmail.com>
2016-03-14 10:41:02 +0100

    Adding various things

--- desktop.nix
+++ desktop.nix
@@ -9,20 +9,26 @@
 		idea = pkgs.callPackage ./nixpkgs/pkgs/applications/editors/idea {};		
 		hugo = pkgs.goPackages.hugo.bin // { outputs = ["bin"]; };
 		kismet = pkgs.callPackage ./nixpkgs/pkgs/applications/networking/sniffers/kismet {};
+		docker_compose = pkgs.pythonPackages.docker_compose;
 		godef = pkgs.goPackages.godef.bin // {outputs = ["bin"]; };
 		redshift = pkgs.callPackage ./nixpkgs/pkgs/applications/misc/redshift {
 			pyxdg = pkgs.python3Packages.pyxdg;
 		};
 	};
 	environment.systemPackages = with pkgs;  [
+		google-cloud-sdk
+		docker_compose
 		btrfs-progs
 		mercurial
+		calibre
 		xfce.thunar
 		xfce.thunar_volman
 		tmux
 		nix-repl
 		firefox
 		go
+		jdk
+		gradle25
 		drive
 		hugo
 		goimports
@@ -38,6 +44,5 @@
 		dropbox
 		spotify
 		redshift
-		jdk
  	];
 }
